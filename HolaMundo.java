9fb8e462ecfeec7c9e678fa37e8398f82b85ff40
hector98 <barrioshector13@gmail.com>
2019-10-28 21:28:29 -0600

    Comenzar practica de Java, y Agregar el programa HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+/** 
+*
+* Mostrar por pantalla la frse "Hola Mundo"
+*
+*/
+
+public class HolaMundo //Clase Principal 
+ {
+    public static void main(String[] args)
+    {
+      System.out.println("Hola Mundo!!!!");
+    }
+ }
